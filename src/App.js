62b31d3aceec886c488721ff5de2a3c38e457c2e
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-05 07:25:12 +0100

    style: styling contact page header and paragraph text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
+import ContactMe from './pages/contactMe/ContactMe';
 import LinkTree from './pages/linkTree/LinkTree';
 
 function App() {
   return (
     <div className="App">
-      <LinkTree />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LinkTree />} />
+          <Route path='/contact' element={<ContactMe />} />
+        </Routes>
+      </BrowserRouter>
+        <Footer />
     </div>
   );
 }
--- src/pages/contactMe/ContactMe.jsx
+++ src/pages/contactMe/ContactMe.jsx
@@ -4,7 +4,14 @@ import './contactme.css';
 function ContactMe() {
     return (
         <div className='contactme'>
+            <div className="container">
+                <h2>Contact Me</h2>
+                <p>Hi there, contact me to ask about anything you have in mind.</p>
+                <form>
 
+                    
+                </form>
+            </div>
         </div>
     );
 }
