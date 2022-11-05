61177132cf0b7cbabe1ddb913f62a967d6235bc7
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-05 04:41:26 +0100

    page: created contact me page

---
+++ src/pages/contactMe/ContactMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './contactme.css';
+
+function ContactMe() {
+    return (
+        <div className='contactme'>
+
+        </div>
+    );
+}
+
+export default ContactMe;
